eca68f0250f78559ecd8837050f8893e39b36e3d
Tracy <Tracyovo_work@outlook.com>
2026-05-04 20:55:50 +0800

    提交

--- release/LiteratureManager/launch.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-cd /d "%~dp0"
-
-start "Literature Manager API" cmd /c start.bat
-start "Literature Manager UI" "Frontend\index.html"
-
-endlocal
--- release/LiteratureManager/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-cd /d "%~dp0"
-
-echo Starting backend...
-cd Backend
-python -m uvicorn app.main:app --host 127.0.0.1 --port 8000 --reload
